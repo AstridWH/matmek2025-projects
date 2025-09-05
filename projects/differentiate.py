8b38ddccd448072ca667a80b2696db81b9b694e7
AstridWH <astrid.wh@gmail.com>
2025-09-05 16:03:36 +0200

    differentiate done, test good

--- projects/differentiate.py
+++ projects/differentiate.py
@@ -5,17 +5,27 @@ def differentiate(u: np.ndarray, dt: float) -> np.ndarray:
     T = len(u)
     tn = np.linspace(0, T*dt, T)
     d = np.zeros(T)
+
     d[0] = (u[1] - u[0])/dt
     d[-1] = (u[-1]- u[-2])/dt
 
-    for i in range(1, len(tn)-2):
+    for i in range(1, len(tn)-1):
         d[i] = (u[i+1] - u[i-1])/(2*dt)
-
+    
     return d
 
 
 def differentiate_vector(u: np.ndarray, dt: float) -> np.ndarray:
-    pass
+    upluss = u[2:]
+    uminus = u[:-2]
+    d = np.zeros(len(u))
+
+    d[0] = (u[1] - u[0])/dt
+    d[-1] = (u[-1]- u[-2])/dt
+    d[1:-1] = (upluss - uminus)/(2*dt)
+
+    return d
+
 
 def test_differentiate():
     t = np.linspace(0, 1, 10)
